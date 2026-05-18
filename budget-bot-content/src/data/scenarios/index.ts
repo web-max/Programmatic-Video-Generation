fc84e0c30ff8f4b43a95e10c78b2227786bc4174
Gerald <gerald.dobin@gmail.com>
2026-05-18 13:00:37 -0500

    Created the Luis Vuitton girlfriend scenario

--- budget-bot-content/src/data/scenarios/index.ts
+++ budget-bot-content/src/data/scenarios/index.ts
@@ -1,4 +1,5 @@
 import { Scenario } from '../types';
 import { bottlesNight } from './bottles-night';
+import { lvBag } from './lv-bag';
 
-export const scenarios: Scenario[] = [bottlesNight];
+export const scenarios: Scenario[] = [bottlesNight, lvBag];
